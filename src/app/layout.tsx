174fd54b76a128948718a039937c71866f962f52
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2026-03-26 11:55:02 +0100

    Add favicon metadata and icon links to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,21 @@ export const metadata: Metadata = {
     creator: "@cenkkorkmaz",
   },
 
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
+
   robots: {
     index: true,
     follow: true,
